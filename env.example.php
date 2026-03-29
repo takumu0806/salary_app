f9910548a6ff30e3ef1510ad54be630578aaced6
AokiTakumu <s2322072@stu.musashino-u.ac.jp>
2026-03-30 03:43:37 +0900

    env.example.php作成

---
+++ env.example.php
@@ -0,0 +1,8 @@
+<?php
+// env.example.php
+// このファイルを env.php にコピーして、自分の環境に合わせて書き換えてください
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'YOUR_DATABASE_NAME');
+define('DB_USER', 'YOUR_USERNAME');
+define('DB_PASS', 'YOUR_PASSWORD');
